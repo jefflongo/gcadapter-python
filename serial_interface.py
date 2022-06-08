7fe169c038741e92d2be5beedc2ab34d81c6533b
jlongo <jeffrey.longo@caci.com>
2022-06-08 14:07:34 -0700

    add rumble

--- serial_interface.py
+++ serial_interface.py
@@ -10,6 +10,8 @@ class SerialInterface:
     def __init__(self):
         self.statuses: list[GCControllerStatus] = []
         self.origins: list[GCControllerStatus] = []
+        self.rumble = [False, False, False, False]
+        self.rumble_needs_update = threading.Event()
 
         try:
             self.adapter = GCAdapter()
@@ -19,6 +21,7 @@ class SerialInterface:
         self.adapter.start_polling()
 
         self.poll_thread = threading.Thread(target=self._poll).start()
+        self.rumble_thread = threading.Thread(target=self._update_rumble).start()
 
     def _poll(self) -> None:
         while True:
@@ -26,8 +29,16 @@ class SerialInterface:
             # yield
             time.sleep(0)
 
+    def _update_rumble(self) -> None:
+        self.rumble_needs_update.wait()
+        self.adapter.set_rumble(*self.rumble)
+
     def get_origin(self, chan: int) -> GCControllerStatus:
         return self.origins[chan]
 
     def get_status(self, chan: int) -> GCControllerStatus:
         return self.statuses[chan]
+
+    def set_rumble(self, chan: int, value: bool) -> None:
+        self.rumble[chan] = value
+        self.rumble_needs_update.set()
