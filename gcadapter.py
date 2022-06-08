c884655e31e7dd1c3516b180fa420fa80487b295
jefflongo <jeff.a.longo@gmail.com>
2022-06-07 21:54:32 -0700

    get all commands working and cleaned up

--- gcadapter.py
+++ gcadapter.py
@@ -1,43 +1,39 @@
-from typing import NamedTuple
+from __future__ import annotations
 
 import usb.core
 import usb.util
 
 
-class GCControllerOrigin(NamedTuple):
+class GCControllerStatus:
     STICK_DEFAULT_VALUE = 0x80
+    SHOULDER_DEFAULT_VALUE = 0
 
-    a: bool = False
-    b: bool = False
-    x: bool = False
-    y: bool = False
-    start: bool = False
-    d_left: bool = False
-    d_right: bool = False
-    d_down: bool = False
-    d_up: bool = False
-    z: bool = False
-    r: bool = False
-    l: bool = False
+    def __init__(self):
+        self.connected: bool = False
 
-    joystick_x: int = STICK_DEFAULT_VALUE
-    joystick_y: int = STICK_DEFAULT_VALUE
-    c_stick_x: int = STICK_DEFAULT_VALUE
-    c_stick_y: int = STICK_DEFAULT_VALUE
-    l_analog: int = 0
-    r_analog: int = 0
+        self.a: bool = False
+        self.b: bool = False
+        self.x: bool = False
+        self.y: bool = False
+        self.start: bool = False
+        self.d_left: bool = False
+        self.d_right: bool = False
+        self.d_down: bool = False
+        self.d_up: bool = False
+        self.z: bool = False
+        self.r: bool = False
+        self.l: bool = False
 
-
-class GCControllerStatus(GCControllerOrigin):
-    connected: bool = False
-
-    use_origin: bool = False
-    get_origin: bool = False
-    err_stat: bool = False
-    err_latch: bool = False
+        self.joystick_x: int = self.STICK_DEFAULT_VALUE
+        self.joystick_y: int = self.STICK_DEFAULT_VALUE
+        self.c_stick_x: int = self.STICK_DEFAULT_VALUE
+        self.c_stick_y: int = self.STICK_DEFAULT_VALUE
+        self.l_analog: int = self.SHOULDER_DEFAULT_VALUE
+        self.r_analog: int = self.SHOULDER_DEFAULT_VALUE
 
 
 class GCAdapter:
+    MAX_CONTROLLERS = 4
     DEFAULT_TIMEOUT = 16
 
     def __init__(self):
@@ -50,7 +46,6 @@ class GCAdapter:
         if self.dev.is_kernel_driver_active(0):
             try:
                 self.dev.detach_kernel_driver(0)
-                print("kernel driver detached")
             except usb.core.USBError as e:
                 raise IOError("Could not detach kernel driver: %s" % str(e)) from e
         self.dev.reset()
@@ -78,79 +73,85 @@ class GCAdapter:
             raise IOError("Could not find endpoints")
 
     def start_polling(self) -> None:
-        self.out_ep.write([0x13], timeout=self.DEFAULT_TIMEOUT)
+        if self.out_ep.write([0x13], timeout=self.DEFAULT_TIMEOUT) != 1:
+            raise IOError
 
     def stop_polling(self) -> None:
-        self.out_ep.write([0x14], timeout=self.DEFAULT_TIMEOUT)
-        return self.in_ep.read(self.in_ep.wMaxPacketSize, timeout=self.DEFAULT_TIMEOUT)
+        if self.out_ep.write([0x14], timeout=self.DEFAULT_TIMEOUT) != 1:
+            raise IOError
 
-    def get_origins(self):
-        self.out_ep.write([0x12], timeout=self.DEFAULT_TIMEOUT)
         bytes = self.in_ep.read(self.in_ep.wMaxPacketSize, timeout=self.DEFAULT_TIMEOUT)
+        if (len(bytes)) != 2 or bytes[0] != 0x24:
+            raise IOError
+
+    def get_origins(self) -> list[GCControllerStatus]:
+        if self.out_ep.write([0x12], timeout=self.DEFAULT_TIMEOUT) != 1:
+            raise IOError
+
+        bytes = self.in_ep.read(self.in_ep.wMaxPacketSize, timeout=self.DEFAULT_TIMEOUT)
+        if len(bytes) != 25 or bytes[0] != 0x22:
+            raise IOError
+
         origins = [
-            GCControllerOrigin(),
-            GCControllerOrigin(),
-            GCControllerOrigin(),
-            GCControllerOrigin(),
+            GCControllerStatus(),
+            GCControllerStatus(),
+            GCControllerStatus(),
+            GCControllerStatus(),
         ]
-        for chan in range(4):
-            origins[chan].joystick_x = bytes[1 + chan * 6]
-            origins[chan].joystick_y = bytes[1 + chan * 6 + 1]
-            origins[chan].c_stick_x = bytes[1 + chan * 6 + 2]
-            origins[chan].c_stick_y = bytes[1 + chan * 6 + 3]
-            origins[chan].l_analog = bytes[1 + chan * 6 + 4]
-            origins[chan].r_analog = bytes[1 + chan * 6 + 5]
+        for chan in range(self.MAX_CONTROLLERS):
+            origins[chan].joystick_x = bytes[1 + (chan * 6)]
+            origins[chan].joystick_y = bytes[1 + (chan * 6) + 1]
+            origins[chan].c_stick_x = bytes[1 + (chan * 6) + 2]
+            origins[chan].c_stick_y = bytes[1 + (chan * 6) + 3]
+            origins[chan].l_analog = bytes[1 + (chan * 6) + 4]
+            origins[chan].r_analog = bytes[1 + (chan * 6) + 5]
 
         return origins
 
-    def get_status(self):
+    def get_status(self) -> list[GCControllerStatus]:
         bytes = self.in_ep.read(self.in_ep.wMaxPacketSize, timeout=self.DEFAULT_TIMEOUT)
+        if len(bytes) != self.in_ep.wMaxPacketSize or bytes[0] != 0x21:
+            raise IOError
+
         statuses = [
             GCControllerStatus(),
             GCControllerStatus(),
             GCControllerStatus(),
             GCControllerStatus(),
         ]
-        for chan in range(4):
-            buttons: int = bytes[(1 + 9 * chan + 1)] | bytes[(1 + 9 * chan + 1)] << 8
-            statuses[chan].a = buttons & (1 << 0)
-            statuses[chan].b = buttons & (1 << 1)
-            statuses[chan].x = buttons & (1 << 2)
-            statuses[chan].y = buttons & (1 << 3)
-            statuses[chan].start = buttons & (1 << 4)
-            statuses[chan].get_origin = buttons & (1 << 5)
-            statuses[chan].err_latch = buttons & (1 << 6)
-            statuses[chan].err_stat = buttons & (1 << 7)
-            statuses[chan].d_left = buttons & (1 << 8)
-            statuses[chan].d_right = buttons & (1 << 9)
-            statuses[chan].d_down = buttons & (1 << 10)
-            statuses[chan].d_up = buttons & (1 << 11)
-            statuses[chan].z = buttons & (1 << 12)
-            statuses[chan].r = buttons & (1 << 13)
-            statuses[chan].l = buttons & (1 << 14)
-            statuses[chan].use_origin = buttons & (1 << 15)
-            statuses[chan].joystick_x = bytes[1 + 9 * chan + 3]
-            statuses[chan].joystick_y = bytes[1 + 9 * chan + 3]
-            statuses[chan].c_stick_x = bytes[1 + 9 * chan + 4]
-            statuses[chan].c_stick_y = bytes[1 + 9 * chan + 5]
-            statuses[chan].l_analog = bytes[1 + 9 * chan + 7]
-            statuses[chan].r_analog = bytes[1 + 9 * chan + 8]
+        for chan in range(self.MAX_CONTROLLERS):
+            statuses[chan].connected = ((bytes[1 + (9 * chan)] >> 4) & 0x03) > 0
+
+            buttons: int = (
+                bytes[(1 + (9 * chan) + 1)] | bytes[(1 + (9 * chan) + 2)] << 8
+            )
+            statuses[chan].a = (buttons & (1 << 0)) != 0
+            statuses[chan].b = (buttons & (1 << 1)) != 0
+            statuses[chan].x = (buttons & (1 << 2)) != 0
+            statuses[chan].y = (buttons & (1 << 3)) != 0
+            statuses[chan].d_left = (buttons & (1 << 4)) != 0
+            statuses[chan].d_right = (buttons & (1 << 5)) != 0
+            statuses[chan].d_down = (buttons & (1 << 6)) != 0
+            statuses[chan].d_up = (buttons & (1 << 7)) != 0
+            statuses[chan].start = (buttons & (1 << 8)) != 0
+            statuses[chan].z = (buttons & (1 << 9)) != 0
+            statuses[chan].r = (buttons & (1 << 10)) != 0
+            statuses[chan].l = (buttons & (1 << 11)) != 0
+
+            statuses[chan].joystick_x = bytes[1 + (9 * chan) + 3]
+            statuses[chan].joystick_y = bytes[1 + (9 * chan) + 3]
+            statuses[chan].c_stick_x = bytes[1 + (9 * chan) + 4]
+            statuses[chan].c_stick_y = bytes[1 + (9 * chan) + 5]
+            statuses[chan].l_analog = bytes[1 + (9 * chan) + 7]
+            statuses[chan].r_analog = bytes[1 + (9 * chan) + 8]
 
         return statuses
 
-    def set_rumble(self, r1, r2, r3, r4) -> None:
-        self.out_ep.write(
-            [0x11, 1 if r1 else 0, 1 if r2 else 0, 1 if r3 else 0, 1 if r4 else 0]
-        )
-
-
-if __name__ == "__main__":
-    import time
-
-    adapter = GCAdapter()
-    adapter.start_polling()
-    time.sleep(1)
-    adapter.get_status()
-    time.sleep(1)
-    ret = adapter.stop_polling()
-    ret = adapter.get_origins()
+    def set_rumble(self, r1: bool, r2: bool, r3: bool, r4: bool) -> None:
+        if (
+            self.out_ep.write(
+                [0x11, 1 if r1 else 0, 1 if r2 else 0, 1 if r3 else 0, 1 if r4 else 0]
+            )
+            != 1
+        ):
+            raise IOError
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import threading
 import time
 import sys
 
-from gcadapter import GCAdapter, GCControllerStatus, GCControllerOrigin
+from gcadapter import GCAdapter, GCControllerStatus
 
 POLL_RATE_HZ = 1
 
@@ -23,5 +23,8 @@ except IOError:
 adapter.start_polling()
 
 while True:
-
+    adapter.stop_polling()
+    ret = adapter.get_origins()
+    adapter.start_polling()
+    adapter.get_status()
     time.sleep(1 / POLL_RATE_HZ)
